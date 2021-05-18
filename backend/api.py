08f1be114ca8017e3804b0ed87ef62ab0bcb15d1
mehmetpekcan <pekcanmehmett@gmail.com>
2021-05-19 00:14:11 +0300

    Config for flask deployment

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import steganography
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='')
 CORS(app)
 
 @app.route("/encode", methods=['POST'])
@@ -25,3 +25,10 @@ def decode():
 	response = { "status": requestObj['status'], 'data': requestObj['data'] }
 
 	return response
+
+@app.route("/are-you-alive", methods=['GET'])
+def are_you_alive():
+	return 'Yes, we are alive'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", threaded=True, port=5000)
