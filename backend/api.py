51913055f5bca3c848a60af0908fa8ef098ccc48
mehmetpekcan <pekcanmehmett@gmail.com>
2021-05-19 11:49:06 +0300

    Test heroku

--- backend/api.py
+++ backend/api.py
@@ -1,35 +1,31 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-# It handles importing problem to import our local directory
-import sys
-sys.path.insert(1, 'backend')
-
-from .steganography import encode_image, decode_image
+# from .steganography import encode_image, decode_image
 
 app = Flask(__name__, static_folder='build', static_url_path='')
 CORS(app)
 
-@app.route("/encode", methods=['POST'])
-def encode():
-	file = request.files['file']
-	secretKey = request.form['secretKey']
-	secretMessage = request.form['secretMessage']
+# @app.route("/encode", methods=['POST'])
+# def encode():
+# 	file = request.files['file']
+# 	secretKey = request.form['secretKey']
+# 	secretMessage = request.form['secretMessage']
 
-	requestObj = encode_image(file, secretMessage, secretKey)
-	response = { "status": requestObj['status'], 'data': requestObj['data'] }
+# 	requestObj = encode_image(file, secretMessage, secretKey)
+# 	response = { "status": requestObj['status'], 'data': requestObj['data'] }
 
-	return response
+# 	return response
 
-@app.route("/decode", methods=['POST'])
-def decode():
-	file = request.files['file']
-	secretKey = request.form['secretKey']
+# @app.route("/decode", methods=['POST'])
+# def decode():
+# 	file = request.files['file']
+# 	secretKey = request.form['secretKey']
 
-	requestObj = decode_image(file, secretKey)
-	response = { "status": requestObj['status'], 'data': requestObj['data'] }
+# 	requestObj = decode_image(file, secretKey)
+# 	response = { "status": requestObj['status'], 'data': requestObj['data'] }
 
-	return response
+# 	return response
 
 @app.route("/are-you-alive", methods=['GET'])
 def are_you_alive():
