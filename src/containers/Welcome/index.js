dfa87aa83a08aff28bfb5f5f724f35ffa2a58301
mehmetpekcan <pekcanmehmett@gmail.com>
2021-05-19 22:45:20 +0300

    Change welcome section;

--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -2,26 +2,31 @@ import { Row, Col, Typography, Alert } from "antd";
 
 import * as S from "./style";
 
-const { Title, Paragraph } = Typography;
+const { Title, Paragraph, Link } = Typography;
 
 function Index() {
   return (
     <Row style={{ marginBottom: 24 }}>
       <Col span={24}>
         <S.MainTitle>Fractal Image Steganography</S.MainTitle>
-        <Title level={3}>How it works?</Title>
+        <Title level={3}>Behind the Scene</Title>
         <Paragraph>
-        We convert the text to char and the image to RGB format to embed the text in the picture.
-        By giving a secret key to the end of the data to be stored, we decide where the data will end.
-        In this way, we can embed the text in the picture.
-        To decode the picture and read the text in it, it will be enough to know the secret key.
+          App embeds message and key to the image. To encode, it changing the
+          some pixels of image with the message and key pixels. To encode, it
+          looking the key pixels and when it is find it decodes the message from
+          it. To do that it converts all message, key and image information to
+          binary format. For furthermore information please visit:&nbsp;
+          <Link
+            href="https://github.com/supremepanda/fractal-image-steganography#understanding-the-theory-of-backend-side"
+            target="_blank"
+          >
+            here
+          </Link>
         </Paragraph>
         <Alert
-          message="Informational Notes"
-          description="We first need to select an image.
-                      Then we must enter out message into 'Secret Message' input.
-                      Finally we must determine a 'Secret Key' and click on the Encode button.
-                      To Decode our image, firstly we must pick a picture which has a secret message in it. After that we just need to enter 'Secret Key' and click on Decode button."
+          message="How it works?"
+          description="Select/Drag & Drop your image to decode or encode. Enter your `secret message`
+          and `secret key` for encoding. And keep the `secret key` for decoding the message for later"
           type="info"
           showIcon
         />
